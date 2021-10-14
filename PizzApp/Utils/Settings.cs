c6dbe184d37dbf58b04e3430b8222d8f5aa008bb
luigi <luigi.degiacomo@gmail.com>
2021-10-14 19:38:10 +0200

    tech

--- PizzApp/Utils/Settings.cs
+++ PizzApp/Utils/Settings.cs
@@ -9,10 +9,10 @@ namespace PizzApp.Utils
         public static string PUBLIC_PARTITION = "PUBLIC";
 
         public static string[] IMG_PATH = {
-            "https://dl.dropboxusercontent.com/s/nzrso2xdyusnual/1.png?dl=0",
-            "https://dl.dropboxusercontent.com/s/ywbmlnnik0froap/2.png?dl=0",
-            "https://dl.dropboxusercontent.com/s/mj0so4s89jgw7sm/3.png?dl=0",
-            "https://dl.dropboxusercontent.com/s/j9d4fak3n0xkpkp/4.png?dl=0"
+            "https://www.talkapp.it/pizzapp/1.png",
+            "https://www.talkapp.it/pizzapp/2.png",
+            "https://www.talkapp.it/pizzapp/3.png",
+            "https://www.talkapp.it/pizzapp/4.png"
         };        
     }
 
